b964f17348b589fe848362bc86fa0dfb4c2450e8
xt9411 <u0965459246@gmail.com>
2025-04-28 10:21:48 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,9 @@ let capture;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  capture = createCapture(VIDEO);
+    capture = createCapture(VIDEO, (stream) => {
+    console.log('攝影機已啟動');
+  });
   capture.size(windowWidth * 0.8, windowHeight * 0.8);
   capture.hide(); // 確保隱藏 DOM 元素
 
@@ -11,9 +13,10 @@ function setup() {
 
 function draw() {
   background('#0096c7');
-  if (capture.loadedmetadata) { // 確保攝影機已準備好
+  // 移除 loadedmetadata 檢查，直接顯示影像
+
     image(capture, (windowWidth - capture.width) / 2, (windowHeight - capture.height) / 2);
-  }
+  
 }
 
 function windowResized() {
